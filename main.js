3a8f9a4f1c95f5ffcb7cef97efcd3faa14be75c4
marianahama <marianahmartello@hotmail.com>
2023-04-20 15:46:44 -0300

    ultima aula

--- main.js
+++ main.js
@@ -23,7 +23,22 @@ $('#botao-cancelar').click(function (){
 })
 
  $('form').on('submit', function(e) {
-    console.log('submit');
     e.preventDefault();
+    const endereçoDaNovaImagem= $('#endereco-imagem-nova').val(); 
+   const novoItem= $('<li style="display:none"></li>');
+   $(`<img src="${endereçoDaNovaImagem}" />`).appendTo(novoItem);
+   $(`
+   <div class= "overlay">
+   <a href= "${endereçoDaNovaImagem}" target= "_blank" title="Ver imagem em tamanho real">
+          Ver imagem em tamanho real
+      </a>
+   </div>
+   `).appendTo(novoItem);
+   $(novoItem).appendTo('ul');
+   $(novoItem).fadeIn(1000);
+   $('#endereco-imagem-nova').val('')
  })
 })
+
+
+
